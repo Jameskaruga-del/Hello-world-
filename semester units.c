3b76e9820adf526921a437a0b80f7896413d02b8
James Karuga <jamesmanyara726@gmail.com>
2025-09-25 23:40:38 +0300

    Add files via upload
    C program to show semester units

---
+++ semester units.c
@@ -0,0 +1,19 @@
+//simple c program (Hello World ) single line comment 
+/*
+Name:James
+Reg No:22
+Date:
+Description: Hello world program 
+*/
+#include<stdio.h>
+// main function 
+int main(){
+    printf("SPC 2101 introduction to programming and problem solving\n");
+    printf("SPC 2102 Introduction to computer architecture\n");
+    printf("SPH 2101 physics\n");
+    printf("SPH 2107 analogue electronic s\n");
+    printf("SPM 2100 dicrete mathematics\n");
+    printf("SPM 2104 mathematical for science\n");
+    printf("UCU 2105 fundamentals of ict\n");
+    return 0;
+}
