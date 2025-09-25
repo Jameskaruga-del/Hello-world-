1639a8c5d72d7fd73018d90268b0fd9208e8e381
James Karuga <jamesmanyara726@gmail.com>
2025-09-25 23:42:57 +0300

    Add files via upload
    A c program to prompt user to enter height,, phone number and bank balance

---
+++ Assignment 2.c
@@ -0,0 +1,29 @@
+//simple c program (Hello World ) single line comment 
+/*
+Name:James
+Reg No:CT101/G/26449/25
+Date:
+Description: Hello world program 
+*/
+#include<stdio.h>
+// main function 
+int main(){
+int height;
+int balance;
+int number;
+//variable declaration 
+    int age;
+    printf("Enter your height:");
+    scanf("%d\n",&height);//take user input
+    //display height 
+    
+    printf("Enter your bank balance:");
+    scanf("%d\n",&balance);//take user input 
+    //display balance
+    
+    printf("Enter your phone number:");
+    scanf("%d\n",&number);//take user input 
+    //display number 
+    
+    return 0;
+}
