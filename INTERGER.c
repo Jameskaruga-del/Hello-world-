a052ea43b2767c70adf002bc90815029edf80f9c
James Karuga <jamesmanyara726@gmail.com>
2025-10-09 22:48:47 +0300

    Add files via upload

---
+++ INTERGER.c
@@ -0,0 +1,27 @@
+/*
+Name:JAMES KARUGA
+Reg No:CT101/G/26449/25
+Date:6/10/2025
+
+Description:a simple c program using do while loop
+
+*/
+
+#include<stdio.h>
+
+int main () {
+
+    int i;
+    
+    for ( i=100;  i>=50; i--){
+    
+    
+    printf("%d\n",i);
+    
+    }
+ 
+    
+    
+    return 0;
+    
+}
